be735384be38d4d01c3d4b4e9be8aa9ac481cba9
synthresin <synthresin@me.com>
2012-10-05 14:46:13 +0900

    액션을 root 에다가 넣어보자

--- js/app.js
+++ js/app.js
@@ -2,11 +2,18 @@ var App = Em.Application.create();
 
 App.Router = Em.Router.extend({
 	root: Em.Route.extend({
+		// 모든 상태에서 통용되는(?) 액션들
+		showIndex: Ember.Route.transitionTo('index'),
+		showDiary: Ember.Route.transitionTo('diary'),
+		showMusic: Ember.Route.transitionTo('music'),
+		showPhoto: Ember.Route.transitionTo('photo'),
+
+
 		index: Em.Route.extend({
 			route: '/',
 			connectOutlets: function(router) {
 				console.log("인덱스 라우트에 들어갔다!");
-				router.get('applicationController').connectOutlet('diary');
+				router.get('applicationController').connectOutlet('index');
 			}
 		}),
 		diary: Em.Route.extend({
@@ -14,6 +21,20 @@ App.Router = Em.Router.extend({
 			connectOutlets: function(router) {
 				console.log("다이어리 라우트에 들어갔다!");
 				router.get('applicationController').connectOutlet('diary');
+			}
+		}),
+		music: Em.Route.extend({
+			route: '/music',
+			connectOutlets: function(router) {
+				console.log("뮤직 라우트에 들어갔다!");
+				router.get('applicationController').connectOutlet('music');
+			}
+		}),
+		photo: Em.Route.extend({
+			route: '/photo',
+			connectOutlets: function(router) {
+				console.log("포토 라우트에 들어갔다!");
+				router.get('applicationController').connectOutlet('photo');
 			}
 		})
 	})
@@ -24,6 +45,13 @@ App.ApplicationController = Em.Controller.extend();
 App.ApplicationView = Em.View.extend({
 	// templateName: 'application'
 	templateName: 'application'
+});
+
+App.IndexController = Em.Controller.extend();
+
+App.IndexView = Em.View.extend({
+	// templateName: 'application'
+	templateName: 'index'
 });
 
 App.DiaryController = Em.Controller.extend();
@@ -38,6 +66,13 @@ App.MusicController = Em.Controller.extend();
 App.MusicView = Em.View.extend({
 	// templateName: 'application'
 	templateName: 'music'
+});
+
+App.PhotoController = Em.Controller.extend();
+
+App.PhotoView = Em.View.extend({
+	// templateName: 'application'
+	templateName: 'photo'
 });
 
 App.initialize();
